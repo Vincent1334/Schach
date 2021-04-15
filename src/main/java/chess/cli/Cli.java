bc2fc40b6c8983914074e2486d9981bc23d55819
Vincent Schiller <vincent.schiller@gmail.com>
2021-04-15 14:38:31 +0200

    create enterGame(), drawBoard() and clearWindow() function. Update Cli.java

--- src/main/java/chess/cli/Cli.java
+++ src/main/java/chess/cli/Cli.java
@@ -12,7 +12,6 @@ public class Cli {
     static Scanner scan;
     static CoreGame coreGame;
 
-
     /**
      * The entry point of the CLI application.
      *
@@ -24,30 +23,63 @@ public class Cli {
         enterGame();
     }
 
+    /**
+     * User interface to initial the Gamemode
+     */
     public static void init(){
-        clearWindow();
-        System.out.println(" ██████╗██╗  ██╗███████╗███████╗███████╗");
-        System.out.println("██╔════╝██║  ██║██╔════╝██╔════╝██╔════╝");
-        System.out.println("██║     ███████║█████╗  ███████╗███████╗");
-        System.out.println("██║     ██╔══██║██╔══╝  ╚════██║╚════██║");
-        System.out.println("╚██████╗██║  ██║███████╗███████║███████║");
-        System.out.println(" ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝");
-        System.out.println("");
-        System.out.println("Please enter the game mode");
-        System.out.println("1. Start a local game against a friend");
-        System.out.println("2. Start a local game against the computer");
-        System.out.println("3. Start a network game");
-        System.out.print("Your entry: ");
-        int input = scan.nextInt();
-        if(input <= 1 && input <= 3) coreGame = new CoreGame(input);
-        else init();
+        do{
+            clearWindow();
+            System.out.println(" ██████╗██╗  ██╗███████╗███████╗███████╗");
+            System.out.println("██╔════╝██║  ██║██╔════╝██╔════╝██╔════╝");
+            System.out.println("██║     ███████║█████╗  ███████╗███████╗");
+            System.out.println("██║     ██╔══██║██╔══╝  ╚════██║╚════██║");
+            System.out.println("╚██████╗██║  ██║███████╗███████║███████║");
+            System.out.println(" ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝");
+            System.out.println("");
+            System.out.println("Please enter the game mode");
+            System.out.println("1. Start a local game against a friend");
+            System.out.println("2. Start a local game against the computer");
+            System.out.println("3. Start a network game");
+            System.out.print("Your entry: ");
+
+            String input = scan.next();
+            if(input.length() == 1 && input.charAt(0) >= 49 && input.charAt(0) <= 51){
+                coreGame = new CoreGame(input.charAt(0)-48);
+                break;
+            }
+        }while(true);
     }
 
+    /**
+     * Gameloop
+     * Clear Windows, drawBoard and wait for user input
+     * After game the loop ends
+     */
     public static void enterGame(){
-        clearWindow();
-
+        do{
+            clearWindow();
+            drawBoard();
+            System.out.print();
+        }while(true);
     }
 
+    /**
+     * Draw the board into the commandline
+     */
+    public static void drawBoard(){
+        for(int y = 0; y < 8; y++){
+            for(int x = 0; x < 8; x++){
+                System.out.print(8-y);
+                System.out.print(coreGame.getBoard().getFigure(x, y).getSymbol() + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.println(" a b c d e f g h");
+    }
+
+    /**
+     * Clear the commandline Window
+     */
     public static void clearWindow(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
