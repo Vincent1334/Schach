94976876a1f8518c2925efdc8639ca9eac574fd9
Vincent Schiller <vincent.schiller@gmail.com>
2021-04-15 12:29:15 +0200

    Update CoreGame and CLI

--- src/main/java/chess/cli/Cli.java
+++ src/main/java/chess/cli/Cli.java
@@ -1,15 +1,43 @@
 package chess.cli;
 
+import java.util.Scanner;
+
 /**
  * Starting point of the command line interface
  */
 public class Cli {
+
+    static Scanner scan;
+
     /**
      * The entry point of the CLI application.
      *
      * @param args The command line arguments passed to the application
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        init();
+        System.out.println(" ██████╗██╗  ██╗███████╗███████╗███████╗");
+        System.out.println("██╔════╝██║  ██║██╔════╝██╔════╝██╔════╝");
+        System.out.println("██║     ███████║█████╗  ███████╗███████╗");
+        System.out.println("██║     ██╔══██║██╔══╝  ╚════██║╚════██║");
+        System.out.println("╚██████╗██║  ██║███████╗███████║███████║");
+        System.out.println(" ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝");
+        System.out.println("");
+        System.out.println("Please enter the game mode");
+        System.out.println("1. Start a local game against a friend");
+        System.out.println("2. Start a local game against the computer");
+        System.out.println("3. Start a network game");
+
+
+
+
+
+
+
+    }
+
+    public static void init(){
+        scan = new Scanner(System.in);
+
     }
 }
