52c5d3ab0ce4b31f5e72724d4ac1bd848a06bdcd
Lydia Engelhardt <engelhardt.lydia@student.uni-luebeck.de>
2021-04-22 14:32:14 +0200

    update Board

--- src/main/java/chess/model/Board.java
+++ src/main/java/chess/model/Board.java
@@ -12,21 +12,21 @@ public class Board {
 
         for (int team = 0; team <= 1; team++) {
             //create Pawns
-            for(int i = 0; i < 8; i++) board[i][1+team*5] = new Pawn(i, 1+team*5, team);
+            for(int i = 0; i < 8; i++) board[i][1+team*5] = new Pawn(team);
             //create King
-            board[4][team*7] = new King(4, team*7, team);
+            board[4][team*7] = new King(team);
             //create Queen
-            board[3][team*7] = new Queen(3, team*7, team);
+            board[3][team*7] = new Queen(team);
             //create Rook
-            for(int i = 0; i <= 1; i++) board[i*7][team*7] = new Rook(i*7, team*7, team);
+            for(int i = 0; i <= 1; i++) board[i*7][team*7] = new Rook(team);
             //create Bishop
-            for(int i = 0; i <= 1; i++) board[2+i*3][team*7] = new Bishop(2+i*3, team*7, team);
+            for(int i = 0; i <= 1; i++) board[2+i*3][team*7] = new Bishop(team);
             //create Knight
-            for(int i = 0; i <= 1; i++) board[1+i*5][team*7] = new Knight(1+i*5, team*7, team);
+            for(int i = 0; i <= 1; i++) board[1+i*5][team*7] = new Knight(team);
             //create None
             for(int y = 2; y < 6; y++){
                 for(int x = 0; x < 8; x++){
-                    board[x][y] = new None(x, y, 2);
+                    board[x][y] = new None();
                 }
             }
         }
