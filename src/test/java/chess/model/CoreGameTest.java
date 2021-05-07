9f5a2cd41515c8d3a1df4832bedd2660312747dc
Sophia Kuhlmann <sophia.kuhlmann@student.uni-luebeck.de>
2021-05-07 13:01:57 +0200

    add tests for EnPassant and fix output (remove "[" and "]")

--- src/test/java/chess/model/CoreGameTest.java
+++ src/test/java/chess/model/CoreGameTest.java
@@ -19,9 +19,9 @@ public class CoreGameTest {
      * Tests whether the chess move method is correct
      */
     @Test
-    public void testChessMove() {
+    public void testDefaultMove() {
         Move move = new Move(new Position(0,1),new Position(0,2));
-        assertTrue(testGame.chessMove(move),"move is not accepted even if it is correct");
+        assertTrue(testGame.chessMove(move),"default move is not accepted even if it is correct");
 
         PrintWriter printWriter = new PrintWriter(System.out, true);
         for (int y = 0; y < 8; y++) {
@@ -33,7 +33,24 @@ public class CoreGameTest {
         }
         printWriter.println("  a b c d e f g h");
         printWriter.println("");
+    }
 
+    @Test
+    public void testEnPassantMove(){
+        for (int x=0; x<8; x++) {
+            for (int y=0; y<8; y++) {
+                testGame.getBoard().setFigure(x,y,new None());
+            }
+        }
+        testGame.getBoard().setFigure(3,4,new Pawn(0));
+        testGame.getBoard().setFigure(0,0,new King(1)); // otherwise CheckChessMate() would not find any possible move to avoid checkmate and would return "checkMate"
+
+        Pawn blackEnPassantPawn = new Pawn(1);
+        blackEnPassantPawn.setEnPassant(true);
+        testGame.getBoard().setFigure(4,4,blackEnPassantPawn);
+
+        Move move = new Move(new Position(3,4),new Position(4,5));
+        assertTrue(testGame.chessMove(move), "enPassant move is not accepted even if it is correct");
     }
 
 }
