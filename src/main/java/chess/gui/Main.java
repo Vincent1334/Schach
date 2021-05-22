a73d4e2784979705404c9de74fdaeeada32914b8
Lydia Engelhardt <engelhardt.lydia@student.uni-luebeck.de>
2021-05-22 16:21:25 +0200

    added schachbrett1.fxml und schachbrett2.fxml

--- src/main/java/chess/gui/Main.java
+++ src/main/java/chess/gui/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("schachbrett.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("schachbrett2.fxml"));
         Parent root = fxmlLoader.load();
 
         primaryStage.setTitle("Schach");
