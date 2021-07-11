8356f0972693315ed95e152eeadb6f3f33af8527
Vincent Schiller <vincent.schiller@student.uni-luebeck.de>
2021-07-11 18:53:48 +0200

    fix CLI println bug

--- src/main/java/chess/cli/Cli.java
+++ src/main/java/chess/cli/Cli.java
@@ -63,6 +63,7 @@ public class Cli {
      */
     public static void startLoop(){
         do {
+            System.out.println(" ");
             System.out.println(" ██████╗██╗  ██╗███████╗███████╗███████╗");
             System.out.println("██╔════╝██║  ██║██╔════╝██╔════╝██╔════╝");
             System.out.println("██║     ███████║█████╗  ███████╗███████╗");
